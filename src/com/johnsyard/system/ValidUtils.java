b2979fc34cafe41d3d8dfbf4cdbc36d3dcd75a09
xuanzhang <xzha0006@student.monash.edu>
2017-05-21 23:49:15 +1000

    finished

--- src/com/johnsyard/system/ValidUtils.java
+++ src/com/johnsyard/system/ValidUtils.java
@@ -10,7 +10,7 @@ import java.util.regex.Pattern;
 public class ValidUtils {
     private static final double MIN_DEPOSIT_AMOUNT = 10000;
     //check empty
-    public static boolean checkEmpty(String input){
+        public static boolean checkEmpty(String input){
         return input.isEmpty();
     }
 
